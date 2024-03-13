b4826b1b8737b8b086a1d19401b9468221260e46
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-03-13 14:59:20 +0200

    Delete 6- graduation directory

--- 6- graduation/API/medicine.api.js
+++
@@ -1,6 +0,0 @@
-const { addMedicine, getMedicine } = require('../Services/medicine.service');
-
-const router=require('express').Router()
-router.post('/addMedicine',addMedicine);
-router.get('/getMedicine',getMedicine);
-module.exports=router
--- 6- graduation/models/medicine.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose')
-const schema=new mongoose.Schema({
-    name:String,
-    usage:String,
-    dosage:String,
-    sideEffects:String,
-})
-module.exports=mongoose.model('medicine',schema)
