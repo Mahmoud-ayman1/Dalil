6745b401c0e9f9811cd3cbf235ebac2b5c581f06
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-04-28 15:15:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ app.use('/users',require('./APIs/user.api'))
 app.use('/medicine',require('./APIs/medicine.api'))
 app.listen(port, () => console.log(`connected , Example app listening on port ${port}!`))
 mongoose.connect('mongodb+srv://dalil:dalil@cluster0.q42qdrh.mongodb.net/')
+.then(()=>{
+    console.log('db connected');
+})
+.catch((err)=>{
+    console.log(err);
+})
+app.use('*',(req,res)=>{
+    res.json({message:"lef we 2rg3 tany"});
+})
