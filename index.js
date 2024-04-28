0f63f492abf44cc9ebd6d88ea674963b732207ec
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-04-28 15:03:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const port = 3000 || process.env.PORT
+const port = 3000
+app.use(require('cors'));
 app.use(express.json())
-app.use('/medicine',require('./API/medicine.api'))
+app.use('/users',require('./APIs/user.api'))
+app.use('/medicine',require('./APIs/medicine.api'))
 app.listen(port, () => console.log(`connected , Example app listening on port ${port}!`))
 mongoose.connect('mongodb+srv://dalil:dalil@cluster0.q42qdrh.mongodb.net/')
