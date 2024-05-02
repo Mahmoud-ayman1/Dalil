5088a8fab1dbfd82526989a5192fa9ad486cd11c
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-05-02 10:09:30 +0300

    Update user.api.js

--- APIs/user.api.js
+++ APIs/user.api.js
@@ -1,4 +1,4 @@
-const { signUp, signIn, verifyEmail } = require('../Services/user.service');
+const { signUp, signIn, verifyEmail, resetPassword } = require('../Services/user.service');
 const { userValidate } = require('../middleware/validation/user.validation');
 
 const router=require('express').Router();
@@ -6,5 +6,5 @@ const router=require('express').Router();
 router.post('/signUp',userValidate,signUp);
 router.post('/signIn',signIn);
 router.get('/verifyEmail/:token',verifyEmail);
-
+router.put('/resetPassword',userValidate,resetPassword);
 module.exports=router;
