87ed5294261c5f7ba2a872582b84775e986def1a
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-05-23 04:32:41 +0300

    Update medicine.api.js

--- APIs/medicine.api.js
+++ APIs/medicine.api.js
@@ -1,6 +1,7 @@
-const { addMedicine, getMedicine } = require('../Services/medicine.service');
+const { addMedicine, getMedicine, getMedicineByCategory } = require('../Services/medicine.service');
 
 const router=require('express').Router()
 router.post('/addMedicine',addMedicine);
 router.get('/getMedicine',getMedicine);
+router.get('/getMedicineByCategory',getMedicineByCategory);
 module.exports=router
