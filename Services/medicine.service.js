5dfb254200ddacf52a717e5f3f30a6e0595084f0
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-06-19 00:51:51 +0300

    Update medicine.service.js

--- Services/medicine.service.js
+++ Services/medicine.service.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+const FormData = require('form-data');
 const medicineModel=require('../models/medicine.model')
 module.exports.getMedicine=async(req,res)=>{
     const {name}=req.body;
@@ -32,3 +34,33 @@ module.exports.getMedicineByPrefix=async(req,res)=>{
     let medicines=await medicineModel.find({name:{$regex:new RegExp(`^${prefix}`)}});
     res.json({statue:true,data:medicines});
 }
+module.exports.getFromImage=async(req,res) => {
+    try {
+        const formData = new FormData();
+        formData.append('file', req.file.buffer, req.file.originalname);
+        const response = await axios({
+            method: 'POST',
+            url: 'https://detect.roboflow.com/detecting-letters/9',
+            params: {
+                api_key: 'K0VfldUkYwC5kYI8Ulv0'
+            },
+            data: formData,
+            headers: {
+                ...formData.getHeaders()
+            }
+        });
+        let semiFinalResponse=[];
+        let arr=response.data.predictions;
+        arr.forEach(function(dic, index) {
+            semiFinalResponse.push([dic['x'],dic['class']]);
+        });
+        semiFinalResponse.sort();
+        let finalResponse="";
+        semiFinalResponse.forEach(function(dic, index) {
+            finalResponse+=dic[1];
+        });
+        res.json({statue:true,data:finalResponse});
+    } catch (error) {
+        res.json({ statue:false,error: error.message });
+    }
+};
