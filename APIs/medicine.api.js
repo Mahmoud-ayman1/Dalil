2783a528a985eb73cba078f7c4512d7b46339059
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-06-19 00:51:36 +0300

    Update medicine.api.js

--- APIs/medicine.api.js
+++ APIs/medicine.api.js
@@ -1,8 +1,10 @@
-const { addMedicine, getMedicine, getMedicineByCategory, getMedicineByPrefix } = require('../Services/medicine.service');
-
+const { addMedicine, getMedicine, getMedicineByCategory, getMedicineByPrefix, getFromImage } = require('../Services/medicine.service');
+const multer = require('multer');
+const upload = multer(); 
 const router=require('express').Router()
 router.post('/addMedicine',addMedicine);
 router.get('/getMedicine',getMedicine);
 router.get('/getMedicineByCategory',getMedicineByCategory);
 router.get('/getMedicineByPrefix',getMedicineByPrefix);
+router.post('/getFromImage',upload.single('image'),getFromImage)
 module.exports=router
