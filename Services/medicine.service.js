0686af2905117a1faa1a165063bf10acb59e1ec3
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-06-24 10:10:08 +0300

    Update medicine.service.js

--- Services/medicine.service.js
+++ Services/medicine.service.js
@@ -34,13 +34,28 @@ module.exports.getMedicineByPrefix=async(req,res)=>{
     let medicines=await medicineModel.find({name:{$regex:new RegExp(`^${prefix}`)}});
     res.json({statue:true,data:medicines});
 }
+function longestCommonSubsequenceLength(str1, str2) {
+    const m = str1.length;
+    const n = str2.length;
+    const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[m][n];
+}
 module.exports.getFromImage=async(req,res) => {
     try {
         const formData = new FormData();
         formData.append('file', req.file.buffer, req.file.originalname);
         const response = await axios({
             method: 'POST',
-            url: 'https://detect.roboflow.com/detecting-letters/9',
+            url: 'https://detect.roboflow.com/detecting-letters/12',
             params: {
                 api_key: 'K0VfldUkYwC5kYI8Ulv0'
             },
@@ -59,6 +74,26 @@ module.exports.getFromImage=async(req,res) => {
         semiFinalResponse.forEach(function(dic, index) {
             finalResponse+=dic[1];
         });
+        let medi=finalResponse;
+        let all=await medicineModel.find();
+        let mx=0,mx2=10000;
+        all.forEach(function(med,index){
+            if(typeof med['name']!=="undefined"){
+                let x= longestCommonSubsequenceLength(medi,med['name']);
+                let w=medi.length+med['name'].length-(2*x);
+                if(x>mx){
+                    finalResponse=med['name'];
+                    mx=x;
+                    mx2=w;
+                }else if(x==mx){
+                    if(w<mx2){
+                        finalResponse=med['name'];
+                        mx=x;
+                        mx2=w;
+                    }
+                }
+            }
+        })
         res.json({statue:true,data:finalResponse});
     } catch (error) {
         res.json({ statue:false,error: error.message });
